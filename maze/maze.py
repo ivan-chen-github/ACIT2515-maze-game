a3615020eb848b2be87980c20d4ba6d36658c035
unknown <edwardto@live.ca>
2021-02-02 17:35:29 -0800

    added some info - Edward

--- maze/maze.py
+++ maze/maze.py
@@ -1,3 +1,39 @@
 class maze:
-    def __init__(filename):
+    def __init__(self, filename):
+        """
+        code documentation goes here
+        """
+
+        """ function that reads maze.txt and creates nested list? """
         pass
+    
+    def check(self, line, column):
+        """
+        finds empty space or wall based on line number and column number
+        param line: description here
+        param column: description here
+        return: coordinates (line number, column number)
+        """
+
+        """ nestedlist[line] by nestedlist[column] is a space (True) or and X (False) """
+        pass
+    
+    def display(self):
+        """
+        displays the maze
+        code documentation
+        """
+
+        """ for line in nestedlist, for i in line, print i """
+        pass
+
+    def find_random_spot(self):
+        """
+        finds a random empty space in the maze
+        code documentation
+        """
+
+        pass
+
+
+
