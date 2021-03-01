7f1adbce71ed0280950f0fb41d8d5776c93c5e54
Ivan <ichen48@my.bcit.ca>
2021-02-28 22:51:28 -0800

    Added win/loss on reaching exit

--- maze/main.py
+++ maze/main.py
@@ -20,32 +20,37 @@ def main():
     maze = Maze("maze.txt")
     player = Player()
     wall_list = []
+    walls = pygame.sprite.Group()
+    fin = pygame.sprite.Group()
     for y, line in enumerate(maze._layout):
             for x, char in enumerate(line):
                 if char == "x":
-                    new_wall = Wall(x*50, y*50)
-                    wall_list.append(new_wall)
+                    walls.add(Wall(x*50, y*50))
+#                    new_wall = Wall(x*50, y*50)
+#                    wall_list.append(new_wall)
                 if char == "e":
-                    finish = Finish(x*50, y*50)
+                    fin.add(Finish(x*50, y*50))
                 if char == "p":
                     player.rect.x = x*50
                     player.rect.y = y*50
     item_locs = []
     item_list = []
-    items = 0
-    while items < 4:
+    item_count = 0
+    items = pygame.sprite.Group()
+    while item_count < 4:
         loc = maze.find_random_spot()
         if loc not in item_locs:
             
             item_locs.append(loc)
             x, y = loc
             new_item = Item(x*50, y*50)
+            items.add(new_item)
             item_list.append(new_item)
-            items += 1
+            item_count += 1
     cd = False
 
     arial = pygame.font.SysFont('arial', 45)
-
+    item_get = 0
     running = True
     while running:
         window.fill((255, 255, 255))
@@ -71,18 +76,36 @@ def main():
             cd = True
 
 
-        text = f"Debug: {cd}"
+        
+        if pygame.sprite.spritecollide(player, items, dokill=True):
+            item_get +=1
+        if pygame.sprite.spritecollide(player, fin, dokill=True) and item_get == 4:
+            win = True
+            running = False
+        elif pygame.sprite.spritecollide(player, fin, dokill=True):
+            win = False
+            running = False
+
+        text = f"Debug: {cd}, also Items obtained: {item_get}"
         text_surface = arial.render(text, True, (0, 0, 0))
         window.blit(text_surface, (0, 950))
-
         #draw everything. This stuff should probably be in a view
+
+        
         window.blit(player.image, player.rect)
-        window.blit(finish.image, finish.rect)
-        for item in item_list:
-            window.blit(item.image, item.rect)
-        for wall in wall_list:
-            window.blit(wall.image, wall.rect)
+#        window.blit(finish.image, finish.rect)
+#        for item in item_list:
+#            window.blit(item.image, item.rect)
+        items.draw(window)
+        walls.draw(window)
+        fin.draw(window)
+#        for wall in wall_list:
+#            window.blit(wall.image, wall.rect)
         pygame.display.update()
+    if win == True:
+        print("Win")
+    elif win == False:
+        print("Lose")
 
 if __name__ == "__main__":
     main()
