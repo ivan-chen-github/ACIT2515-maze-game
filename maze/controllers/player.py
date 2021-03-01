547491857ece2ccf45a6e93f52033bbf37b61861
Ivan <ichen48@my.bcit.ca>
2021-03-01 13:46:29 -0800

    Removed debuggers for PlayerController

--- maze/controllers/player.py
+++ maze/controllers/player.py
@@ -41,19 +41,3 @@ class PlayerController():
         if self._diff >= 1000:
             self._diff = 0
             self._cd = False
-        
-
-
-
-"""
-while True: # game loop
-    # the following method returns the time since its last call in milliseconds
-    # it is good practice to store it in a variable called 'dt'
-    dt = clock.tick() 
-
-    time_elapsed_since_last_action += dt
-    # dt is measured in milliseconds, therefore 250 ms = 0.25 seconds
-    if time_elapsed_since_last_action > 250:
-        snake.action() # move the snake here
-        time_elapsed_since_last_action = 0 # reset it to 0 so you can count again
-"""
