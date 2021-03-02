df007cc1905841bb0690e1c7219d6241b0ab4bad
bradleygee8 <71402232+bradleygee8@users.noreply.github.com>
2021-03-01 16:13:00 -0800

    added documentation. do not know what cd and diff

--- maze/controllers/player.py
+++ maze/controllers/player.py
@@ -6,28 +6,38 @@ class PlayerController():
     TILE_PX = 50
     
     def __init__(self, player, maze):
+        """
+        Intializes the controller for the player, so you are able to move the player
+
+        param player: is the play that is in the game
+        type: Player()
+
+        paramm maze: is the maze that the player will be in
+        type: Maze()
+        """
         self._player = player
         self._maze = maze
         self._cd = False
         self._diff = 0
 
     def get_input(self, time):
+        """ gets the input from the user, move the player """
         if self._cd is True:
             self.check_cd(time)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.locals.K_RIGHT] and self._cd is False and self._maze.can_move_to((self._player.rect.x+50)/50, self._player.rect.y/50):
+        keys = pygame.key.get_pressed() #-- the keys that press
+        if keys[pygame.locals.K_RIGHT] and self._cd is False and self._maze.can_move_to((self._player.rect.x+50)/50, self._player.rect.y/50): #-- checks if the they clicked right and if the player can move in the direction
             self._player.rect.x = min(self._player.rect.x + self.TILE_PX, 1000)
             self.set_cd()
 
-        elif keys[pygame.locals.K_LEFT]  and self._cd is False and self._maze.can_move_to((self._player.rect.x-50)/50, self._player.rect.y/50):
+        elif keys[pygame.locals.K_LEFT]  and self._cd is False and self._maze.can_move_to((self._player.rect.x-50)/50, self._player.rect.y/50):#-- checks if the they clicked right and if the player can move in the direction
             self._player.rect.x = max(self._player.rect.x - self.TILE_PX, 0)
             self.set_cd()
 
-        elif keys[pygame.locals.K_UP] and self._cd is False and self._maze.can_move_to(self._player.rect.x/50, (self._player.rect.y-50)/50):
+        elif keys[pygame.locals.K_UP] and self._cd is False and self._maze.can_move_to(self._player.rect.x/50, (self._player.rect.y-50)/50):#-- checks if the they clicked right and if the player can move in the direction
             self._player.rect.y = max(self._player.rect.y - self.TILE_PX, 0)
             self.set_cd()
 
-        elif keys[pygame.locals.K_DOWN] and self._cd == False and self._maze.can_move_to(self._player.rect.x/50, (self._player.rect.y+50)/50):
+        elif keys[pygame.locals.K_DOWN] and self._cd == False and self._maze.can_move_to(self._player.rect.x/50, (self._player.rect.y+50)/50):#-- checks if the they clicked right and if the player can move in the direction
             self._player.rect.y = min(self._player.rect.y + self.TILE_PX, 1000)
             self.set_cd()
 
