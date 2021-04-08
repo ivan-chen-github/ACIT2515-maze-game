d8792d636808c01ff2c7ede501f2eacfb8e08023
bradleygee8 <71402232+bradleygee8@users.noreply.github.com>
2021-04-08 09:15:55 -0700

    made to_dict part of score

--- maze/models/score.py
+++ maze/models/score.py
@@ -29,25 +29,35 @@ class Score:
         else:
             self._date = date
 
-def from_json(flask_url):
-    """ Reads data from Flask server and creates nested list of scores
+    def to_dict(self):
+        """ Takes Score object and creates a dictionary
+
+        """
+        dict = {
+            "player_name" : self._player_name,
+            "score" : self._score,
+            "date" : self.._date
+        }
+        return dict
+
+"""def from_json(flask_url):
+    Reads data from Flask server and creates nested list of scores
 
     :param flask_url: url of Flask server to get data from
     :type flask_url: str
-    """
+    
     reply = requests.get(flask_url)
     payload = reply.content
     dict_payload = ast.literal_eval(payload.decode("UTF-8"))
     
     return dict_payload
 
-
 def from_dict(dict):
-    """ Reads data from a dictionary and creates object
+    Reads data from a dictionary and creates object
 
     :param dict: dictionary to be read
     :type dict: dict
-    """
+    
     obj =''
     if "date" in dict:
         obj = Score(
@@ -63,22 +73,11 @@ def from_dict(dict):
         return obj
 
 def to_json(score_data):
-    """ Takes score data and serializes into JSON file
+    Takes score data and serializes into JSON file
 
-    """
+    
     with open('scores.json') as json_file:
         data = json.load(json_file)
     with open('scores.json', mode='w') as f:
         data.append(json.loads(score_data))
-        json.dump(data, f)
-
-def to_dict(score):
-    """ Takes Score object and creates a dictionary
-
-    """
-    dict = {
-        "player_name" : score._player_name,
-        "score" : score._score,
-        "date" : score._date
-    }
-    return dict
+        json.dump(data, f)"""
