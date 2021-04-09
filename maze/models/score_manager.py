2aea1d6b2e178798a55a610c46e2bcd7f78252d3
bradleygee8 <71402232+bradleygee8@users.noreply.github.com>
2021-04-08 17:45:13 -0700

    documentation

--- maze/models/score_manager.py
+++ maze/models/score_manager.py
@@ -5,41 +5,51 @@ import ast
 from models.score import Score
 import operator
 class ScoreManager():
+    """
+    Keeps track of all the scores from the game
+    """
     def __init__(self):
-        self._scores = {}
+        self._scores = {} #-- empty dictionary to add the scores
     
     @property
     def scores(self):
+        """
+        scores will return all of the scores in the manager as list
+        
+        return: list of scores
+        return type: list
+        """
         return list(self._scores.values())
     
     def add_score(self, score):
-        """ Add a new score
-
-        Args:
-            Score (ScoreItem): the item to add
-
-        Raises:
-            TypeError: if the item is not a score instance
-
-        Returns:
-            None"""
+        """ 
+        adds the score to the dictionary
+        
+        params score: is the score to add to the dictionary
+        type: score
+        
+        Returns: none
+        """
         
         self._scores[score.player_name] = score
 
     def remove_score(self, score_name):
-        """ Removes score
+        """ 
+        Removes score with the player name
 
-        Args:
-            score_name(str): name of the score you want to remove
-
-        Returns:
-            None
+        param score_name: is the name that you want to delete from the manager
+        type score_name: str
+        
+        Returns: None
         """
         if score_name in self._scores:
             del self._scores[score_name]
     def get_scores(self):
-        """ Will get all of the scores 
+        """ 
+        Will get all of the scores that is in the dictionary
+        
         return: list of scores
+        return type: list
         """
         score_list = []
         for score in self._scores:
@@ -47,22 +57,41 @@ class ScoreManager():
         return score_list
 
     def serialize(self):
+        """
+        will get the list of scores from get_scores() and return the dictionary for it
+        
+        return: dictionary of the scores
+        return type : dictionary
+        """
         print(self.get_scores())
         return {'scores': self.get_scores()}
 
 
     def __len__(self):
-        """Returns the length of the dictionary """
+        """
+        Returns the number of scores that is in the dictionary
+        
+        return: the number of scores
+        return type: int
+        """
         return len(self._scores)
     
     def to_json(self, json_file):
+        """
+        serialize the scores and writes it to json_file
+        
+        param json_file: the name of the file you want to write the scores too
+        type: string
+        
+        return: none
+        """
         convert_list = []
-        for score in self._scores:
+        for score in self._scores: #-- serialize the scores
             convert = {"name": self._scores[score].player_name, 'score' :self._scores[score].score}
             convert_list.append(convert)
         scores = {"scores": convert_list}
         print(scores)
-        with open(json_file,'w') as file:
+        with open(json_file,'w') as file: #-- opens the file and writes the serialized scores to it
             file.write(str(scores))
     
     def from_json(self, flask_url):
@@ -71,6 +100,9 @@ class ScoreManager():
 
         :param flask_url: url of Flask server to get data from
         :type flask_url: str
+        
+        :return dict_payload: is the dict of scores
+        return type: dict
         """
         reply = requests.get(flask_url)
         payload = reply.content
@@ -80,12 +112,20 @@ class ScoreManager():
 
 
     def from_csv (self, csv_file):
-        with open(csv_file, 'r') as file:
-            data = csv.reader(file)
-            data = list(data)
+        """
+        reads a csv files of scores then adds them to the dictionary
+        
+        params csv_file: the name of the file you want to read that has the scores in them
+        type: string
+        
+        return: none
+        """
+        with open(csv_file, 'r') as file: #-- opens the csv file to read
+            data = csv.reader(file) #-- gets the data that is in the file
+            data = list(data) #-- makes it a list
             for item in data:
-                if item[1].isnumeric():
+                if item[1].isnumeric(): #-- checks if the score is numeric
                     print(item[1])
-                    score = Score(item[0], int(item[1]))
-                    self.add_score(score)
+                    score = Score(item[0], int(item[1])) #-- converts the data to a score object
+                    self.add_score(score) #-- ads the score
 
